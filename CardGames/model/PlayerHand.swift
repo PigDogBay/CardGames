152cfd6b15fb128ed813499009681b4263b05a68
Mark Bailey <markpdbailey@gmail.com>
2022-10-05 10:58:53 +0100

    Moved code into Defines

--- CardGames/model/PlayerHand.swift
+++ CardGames/model/PlayerHand.swift
@@ -7,30 +7,6 @@
 
 import Foundation
 
-///Player can swap one card with the middle or all 3
-enum Turn : Equatable {
-    case swap(hand : PlayingCard, middle : PlayingCard)
-    case all
-    
-    func display() -> String {
-        switch self {
-        case .swap(hand: let hand, middle: let middle):
-            return "Swap \(hand.display()) for \(middle.display())"
-        case .all:
-            return "All"
-        }
-    }
-}
-
-struct ScoredTurn {
-    let turn : Turn
-    let score : BragHandScore
-    let middleScore : BragHandScore
-    func display() -> String {
-        return "\(turn.display()) score: \(score.display()), middle: \(middleScore.display())"
-    }
-}
-
 class PlayerHand {
     var hand : [PlayingCard]
     
